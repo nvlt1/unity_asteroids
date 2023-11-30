20527807e9f5ba5d91e61366a79019d4190ad2e2
Nav <navdeepnx@gmail.com>
2023-11-29 19:27:31 -0800

    Asteroids spawning in differect locations, rotations, and angles. Asteroid should spawn at a delayed time and come in at a varianced angle towards the middle

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroids : MonoBehaviour
+{
+
+    // array of sprites to pick a random asteroid in Start()
+    public Sprite[] sprites;
+
+    // this gets randomized when we spawn asteroids, 1.0 just a set value
+    public float size = 1.0f;
+    public float minSize = 0.5f;
+    public float maxSize = 1.5f;
+
+    // references sprite renderer to change which sprite is rendered
+    private SpriteRenderer mySpriteRenderer;
+    //reference to rigid body
+    private Rigidbody2D myRigidBody;
+    
+    private void Awake(){
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
+        myRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mySpriteRenderer.sprite = sprites[Random.Range(0, sprites.Length)];
+
+        //randomizing size and rotation + sprite
+        // eularAngles represent 3 dimensional rotation by doing 3 separate rotations around individual axes
+        this.transform.eulerAngles = new Vector3(0.0f, 0.0f, Random.value * 360.0f); // x, y, z values, z * 360 degrees
+        //add uniqueness to asteroids, changes mass of each object
+        this.transform.localScale = Vector3.one * this.size; // vector3.one is just x, y, z values all = 1 
+
+        myRigidBody.mass = this.size;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+// awake is called when the script obj (asteroid here) is initialized whether or not script is enabled
+// awake is used to set up references between scripts and START is used to pass any information back and forth
---
+++ Assets/Scripts/AsteroidSpawner.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidSpawner : MonoBehaviour
+{
+    // reference to asteroid prefab to instantiate it
+    public AsteroidSpawner asteroidPrefab;
+
+    // variance for asteroids so they don't just attack the center but curve around it
+    public float trajectoryVar = 15.0f;
+    public float spawnDist = 14.0f; // scene at roughly x:11, spawn outside the scene 
+    public float spawnRate = 2.0f; // every 2 second
+    public int spawnAmt = 1;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    //spawn new asteroids at a regular rate
+    // invokes a method every x amount of seconds over and over for asteroid spawns
+    InvokeRepeating(nameof(Spawn),this.spawnRate, this.spawnRate); // y, z = intial time to call this, and how often to repeat
+        
+    }
+
+    private void Spawn(){
+
+        
+        
+        for (int i = 0; i < this.spawnAmt; i++) {
+
+            // RANDOM spawn point
+            // generates random point inside the cirlce or on the circle
+            Vector3 spawnDirection = Random.insideUnitCircle.normalized * this.spawnDist; // normalized sets magnitude to 1 so asteroids spawn on edge of circle
+            Vector3 spawnPoint = this.transform.position + spawnDirection;
+
+            // RANDOM direction
+            float variance = Random.Range(-this.trajectoryVar, this.trajectoryVar);
+            //rotation represented with quaternions
+            Quaternion rotation = Quaternion.AngleAxis(variance, Vector3.forward); // AngleAxis(angle, axis - z axis), .forward is (0, 0, 1)
+
+            // object instantiation
+            AsteroidSpawner asteroid = Instantiate(this.asteroidPrefab, spawnPoint, rotation); // rotation gives it's trajectory as well 
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
